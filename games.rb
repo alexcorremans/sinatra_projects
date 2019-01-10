31fc6766f3686bcd1773c7c60d16c33785a7467d
Alexandra Corremans <alexandra.corremans@gmail.com>
2019-01-10 15:15:59 +1100

    move link back to homepage to layouts require guess_the_word app

--- games.rb
+++ games.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require_relative 'web_guesser/app'
 require_relative 'caesar_cipher/app'
+require_relative 'guess_the_word/app'
 
 enable :sessions
 
